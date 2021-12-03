bfd2204370bef77517e4f2ff41a047c06db601fa
Kartik721 <kartikkounder@gmail.com>
2021-12-03 21:27:32 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
+var name = "WORLD";
+var greet = <p> <h1>HELLO , {name} </h1></p>;
+
+ReactDOM.render(
+  greet,
+  document.getElementById('root')
+);
 
